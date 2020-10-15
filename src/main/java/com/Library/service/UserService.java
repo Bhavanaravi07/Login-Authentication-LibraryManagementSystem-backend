6bc78343c4d2591415f8191b3feb311aef5bb780
gopic <gopic@GOPICHAND>
2020-10-15 16:46:34 +0530

    Login Authentication by P.gopichandh

---
+++ src/main/java/com/Library/service/UserService.java
@@ -0,0 +1,19 @@
+package com.Library.service;
+
+import com.Library.model.User;
+import com.Library.model.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    User save(UserDto user);
+    List<User> findAll();
+    void delete(int id);
+
+    User findOne(String username);
+
+    User findById(int id);
+
+    UserDto update(UserDto userDto);
+}
